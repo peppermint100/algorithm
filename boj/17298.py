849d34c3dbd857206bc3ab06609f1a2f6d067a17
peppermint100 <krpeppermint100@gmail.com>
2022-09-04 19:30:20 +0900

    boj 17289

---
+++ boj/17298.py
@@ -0,0 +1,22 @@
+size = 4
+s1 = "3 5 2 7"
+
+l = list(map(int, s1.split(" ")))
+
+st = [0]
+ans = [-1] * size
+
+for i in range(size):
+    while st:
+        if l[i] > l[st[-1]]:
+            ans[st.pop()] = l[i]
+        else:
+            st.append(i)
+            break
+    
+    if not st:
+        st.append(i)
+
+print(*ans)
+
+        
