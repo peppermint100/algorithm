ab9ac89758ebc0c98c279087312c3832481dbb6d
peppermint100 <krpeppermint100@gmail.com>
2022-09-06 22:01:16 +0900

    - boj 1918

---
+++ boj/1918.py
@@ -0,0 +1,36 @@
+# s1 = "A*(B+C)"
+s1 = "A+B*C"
+
+
+st = []
+ans = []
+
+for i in range(len(s1)):
+    c = s1[i]
+
+    if c.isalpha():
+        ans.append(c)
+    else:
+        if c == "(":
+            st.append(c)
+        elif c == "*" or c == "/":
+            while st and (st[-1] == "*" or st[-1] == "/"):
+                ans.append(st.pop())
+            st.append(c)
+        elif c == "+" or c == "-":
+            while st and st[-1] != "(":
+                ans.append(st.pop())
+            st.append(c)
+        elif c == ")":
+            while st and st[-1] != "(":
+                ans.append(st.pop())
+            st.pop()
+    print("c = ", c)
+    print("st = ", st)
+    print("ans = ", ans)
+
+while st:
+    ans+=st.pop()
+
+
+print("".join(ans))
